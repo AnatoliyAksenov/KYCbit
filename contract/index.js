57bcc8eec4d3b9086ff63d6f0dc365fcaa1e6aff
Anatoliy A Aksenov <anatoliy@aksenov.org>
2016-12-10 18:23:46 +0300

    Add contract + 5

--- contract/index.js
+++ contract/index.js
@@ -6,12 +6,13 @@ var address = process.env.contract_address;
 
 web3.setProvider(new web3.providers.HttpProvider(rpc_url));
 
-var coinbase = web3.eth.coinbase;
 var kycbit = {};
 
 if(web3.isConnected()){
         var abi = [{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"IntIDQuery","outputs":[{"name":"_intID","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"HashToBIK","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"HashToIntID","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"AddressQuery","outputs":[{"name":"_address","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_BIK","type":"string"},{"name":"_hash","type":"bytes32"},{"name":"_intID","type":"string"}],"name":"CustomerInsert","outputs":[{"name":"result","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"isins","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"BIKQuery","outputs":[{"name":"BIK","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"HashToAddress","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[],"type":"constructor","payable":true},{"payable":false,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"}],"name":"Insert","type":"event"}];
         
+        var coinbase = web3.eth.coinbase;
+
         kycbit = web3.eth.contract(abi).at(address);
         
         web3.eth.defaultAccount = web3.eth.accounts[0];
